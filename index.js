0db154ef3ab38c0e6d34f2cdd4c5af0d888df22f
David-Okun <david.okun@ibm.com>
2017-07-20 18:36:40 -0500

    Updated for Bluemix deployment

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var app = express();
+var icloud = require("find-my-iphone").findmyphone;
 
-app.get('/status', function(req, res) {
-    res.statusCode = 200;
-    res.send({"message": "Nice!"});
+icloud.apple_id = process.env.APPLE_ID;
+icloud.password = process.env.APPLE_PASSWORD;
+
+var homeLatitude = process.env.HOME_LATITUDE;
+var homeLongitude = process.env.HOME_LONGITUDE;
+
+app.get('/status/:forceAlert', function(req, res) {
+    var forceAlert = req.params.forceAlert;
+    icloud.getDevices(function(error, devices) {
+        if (error) {
+            res.status = 404;
+            res.send({"error": error});
+        } else {
+            var iPhone = devices.find(o => o.deviceDisplayName === 'iPhone 7 Plus'); // query your devices and choose your label here
+            icloud.getDistanceOfDevice(iPhone, homeLatitude, homeLongitude, function(err, result) {
+                var distance = result.distance.value;
+                if (err) {
+                    res.status = 404;
+                    res.send({"error": err});
+                } else {
+                    if (result.distance.value > 100 || forceAlert == true) {
+                        icloud.alertDevice(iPhone.id, function(err) {
+                            if (err) {
+                                res.status = 404;
+                                res.send({"error": err});
+                            } else {
+                                icloud.getLocationOfDevice(iPhone, function(err, location) {
+                                    if (err) {
+                                        res.status = 404;
+                                        res.send({"error": err});
+                                    } else {
+                                        res.statusCode = 200;
+                                        res.send({"distance": result.distance.value + " meters", "alertSent": true, "address": location});
+                                    }
+                                });
+                                
+                            }
+			            });
+                    } else {
+                        res.statusCode = 200;
+                        res.send({"distance": result.distance.value + " meters", "alertSent": false});
+                    }
+                }
+			});
+        }
+    });
 })
 
 var port = process.env.PORT || 8080
