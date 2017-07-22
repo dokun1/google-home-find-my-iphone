de429ddd1b143660a5baa801ef4f400941cbc1ba
dokun1 <david@okun.io>
2017-07-22 12:12:11 -0500

    Working state, need to figure out private deployment

--- FMIAPI/index.js
+++ FMIAPI/index.js
@@ -16,34 +16,48 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/status', function(req, res) {
     var forceAlert = req.body.forceAlert;
+    if (forceAlert == true) {
+        console.log("Will force an alert");
+    }
     var token = req.body.authToken;
     if (token != authToken || !token) {
+        console.log("Auth token incorrect");
         res.status = 401;
         res.send({"Error": "Incorrect Auth Token"});
     } else {
+        console.log("Getting iCloud Devices");
         icloud.getDevices(function(error, devices) {
         if (error) {
+            console.log("Could not load iCloud devices");
             res.status = 404;
             res.send({"error": error});
         } else {
+            console.log("Found " + devices.length + " devices");
             var iPhone = devices.find(o => o.deviceDisplayName === 'iPhone 7 Plus'); // query your devices and choose your label here
             icloud.getDistanceOfDevice(iPhone, homeLatitude, homeLongitude, function(err, result) {
-                var distance = result.distance.value;
                 if (err) {
+                    console.log("Could not get distance");
                     res.status = 404;
                     res.send({"error": err});
                 } else {
+                    var distance = result.distance.value;
+                    console.log("Distance found: " + result);
                     if (result.distance.value > 100 || forceAlert == true) {
+                        console.log("Distance from device greater than 100 meters - alerting device and getting address.");
                         icloud.alertDevice(iPhone.id, function(err) {
                             if (err) {
+                                console.log("Could not alert device");
                                 res.status = 404;
                                 res.send({"error": err});
                             } else {
+                                console.log("Device alerted");
                                 icloud.getLocationOfDevice(iPhone, function(err, location) {
                                     if (err) {
+                                        console.log("Could not get location of device");
                                         res.status = 404;
                                         res.send({"error": err});
                                     } else {
+                                        console.log("Device located: " + location);
                                         res.statusCode = 200;
                                         res.send({"distance": result.distance.value + " meters", "alertSent": true, "address": location});
                                     }
@@ -52,6 +66,7 @@ app.post('/status', function(req, res) {
                             }
 			            });
                     } else {
+                        console.log("Distance from device less than 100 meters.");
                         res.statusCode = 200;
                         res.send({"distance": result.distance.value + " meters", "alertSent": false});
                     }
--- GoogleAction/index.js
+++ GoogleAction/index.js
@@ -1,18 +1,57 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var promise = require('request-promise');
+var http = require('http');
 
-var authToken = process.env.AUTH_TOKEN;
+var APIAuthToken = process.env.AUTH_TOKEN;
+var microURI = process.env.FMIAPIURI;
 
 app.use(bodyParser.json()); // support json encoded bodies
 
-app.post('/findPhone', function(req, res) {
+function formOutput(body) {
+  var distance = body['distance'];
+  var alertSent = body['alertSent'];
+  var address = body['address'];
+  if (address) {
+    return "Your phone is approximately" + distance + " away from you. The closest address I could find is " + address;
+  } else {
+    return "Your phone is approximately " + distance + " away from you. ";
+  }
+}
 
+app.post('/findPhone', function(req, res) {
+  var alarm = req.body.AlarmEntity;
+  let force = false;
+  if (alarm) {
+    console.log("Will request alarm force");
+    force = true;
+  }
+  var options = {
+    method: 'POST',
+    uri: microURI,
+    body: {
+      authToken: APIAuthToken,
+      forceAlert: force
+    },
+    json: true
+  };
+  console.log("Making POST request to " + microURI);
+  res.setHeader('Content-Type', 'application/json');
+  promise(options).then(function(parsedBody) {
+    // handle proper response
+    console.log("Request successful: " + parsedBody);
+    res.send(JSON.stringify({'speech': formOutput(parsedBody), 'displayText': formOutput(parsedBody)}));
+  }).catch(function(err) {
+    // something failed
+    console.log("An error occurred: " + err);
+    res.send(JSON.stringify({'speech': 'Oops, something bad happened' + err, 'displayText': 'Oops, something bad happened'}));
+  });
 });
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8090
 app.listen(port, function() {
-  console.log('Node Find My iPhone API running on port ' + port);
+  console.log('Node Google Action Webhook for Find My iPhone API running on port ' + port);
 });
 
 exports = module.exports = app;
